c59e38e0f1638af863293eb8c23233c7ab370f1e
Lexomina <67521334+Lexomina@users.noreply.github.com>
2021-02-11 00:06:03 +0300

    New

---
+++ src/app/component-name/component-name.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { ComponentNameComponent } from './component-name.component';
+
+describe('ComponentNameComponent', () => {
+  let component: ComponentNameComponent;
+  let fixture: ComponentFixture<ComponentNameComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ComponentNameComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ComponentNameComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,9 @@
+export const FireBaseConfig = {
+  apiKey: "AIzaSyCM_0p3bAIIzPavswc3pbMzhxtlXxKcGhU",
+  authDomain: "mertoline-89e24.firebaseapp.com",
+  projectId: "mertoline-89e24",
+  storageBucket: "mertoline-89e24.appspot.com",
+  messagingSenderId: "41552831549",
+  appId: "1:41552831549:web:0dc8fcf621c521bffa3654",
+  measurementId: "G-MYWFZFY3L9"
+};
